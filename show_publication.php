f27db6939ae238e1a0fa5c6641c985d0a7b7ec1b
kapil484 <kapil.kr909@gmail.com>
2021-10-28 15:36:56 +0530

    add thesis final

--- show_publication.php
+++ show_publication.php
@@ -25,7 +25,7 @@ include('include/header.php');
     padding: 10px 20px 10px 20px;
     list-style: none;
 }
-.body{
+body{
   text-align:center;
 }
 p{
@@ -70,68 +70,60 @@ tr:nth-child(even) {
                     <a href="thesis_type.php">Thesis Type</a>
                 </li>
             </ul>
-        </div>
+    </div>
     <div class="container-fluid">
         
         <div class="container">
-            <div class="body">
-                  <h3>Representation of the Self: Reading Sports Autobiographies</h3>
-                Chandran, Smitha (2021) Representation of the Self: Reading Sports Autobiographies. PhD thesis.
-                  <br>
-                  <a href="http://ethesis.nitrkl.ac.in/10200/1/2021_PhD_SChandran_513HS3002_Representation.pdf"><img src="application_pdf.png">PDF (Restricted upto 03/06/2023)
-                </a><br>Restricted to Repository staff only<br>
-                906Kb
-                  <h4>Abstract</h4>
-                  <p>
-                    Autobiographies, in general, have been defined as ‘life-writing’ and ‘denotes all modes and genres of telling one’s own life.’ It seeks to reconstruct the author’s personal story within a given ‘social, cultural, and historical framework’. Anybody who can write a sentence or speak to a ghost-writer or speak into a tape-recorder can offer an autobiography (Olney 3). However, lucid the narrative seems, it is the most elusive of all the literary forms as it is challenging for a critic to get a hold on it since there are no available general rules. Along with the elites and the subalterns, the protean nature of autobiographical genre has opened the gates for sportspersons too. Treated as stars, they remain in the public memory for a short time, till they achieve laurels in their sport. Research has established that earlier accounts of a sportsperson’s life lacked subjectivity as they were mere instructions related to their sports. Today it is a genre all by itself, which speaks about the lived experiences of its author in order to build a direct connection between the author and the reader. It stores a vast repository of social and cultural information of the times when the autobiography is penned and brings to life the contemporaneous happenings of the society as observed through the eyes of the narrator. Therefore, we can say that the ‘hero’ and ‘achieved celebrity’ status of a sportsperson provides his autobiography with extraordinary relevance. Sportspersons are perceived as idols who contain the power to influence and contribute a broad cultural significance to contemporary society. Hence unlike novels and biographies, readers carry a certain amount of expectation from the narrator of an autobiography which makes the author’s act transactional and performance oriented. This thesis understands that autobiography is a performance and explores the incidents and memories focused by the narrators to progress their stories. Their autobiographies reveal the unique configurations of memory, identity, and culture very clearly. The notion of identity that emerges is not essentialist rather strategic and positional. The autobiographies that have been selected for analyses are, Milkha Singh’s The Race of My Life, M.C Mary Kom’s Unbreakable, Saina Nehwal’s Playing to Win, Abhinav Bindra’s A Shot at History, Sania Mirza’s Ace against Odds and PT Usha’s The Golden Girl. All these auto-biographers are recipients of the country’s highest sports honours and are trailblazers of their sport. The authors chosen all play individual sports, and they are the only Indian elite sports stars playing individual sport who have written autobiographies till date. These sportspersons have represented India internationally in athletics, boxing, badminton, shooting, and tennis. Other available Indian sports autobiographies are of players playing group sport, especially cricket. In the last decade, the country has witnessed the publication of quite a few cricket autobiographies. Cricket is the most popular sport in India, almost practised as a ‘religion’ and in the process, most of the struggles of the individual sportspersons go unnoticed. The main difference between players playing individual sport and group sport is that in individual sport the success and failure are solely borne by the sportsperson, while in group sport it gets divided among the players. Hence the focus of this study is on autobiographies written by individual sportspersons, by looking at the way they have portrayed themselves to their readers. This thesis argues that Indian sports autobiographies are performances of the sportsperson’s individual choice, athletic identity, and family narrative, and these remain at the centre of their consciousness when they develop their autobiography.
-                  </p>
+          <?php 
+          $id=$_REQUEST['id'];
+
+            $res=$con->query("select * from publication where publication_id='$id'");
+                $i=1;
+                while($row=mysqli_fetch_array($res))
+                {
+   
+                  echo "<h3>".$row['thesis_title']."</h3>";
+                  echo "<h5>".$row['authors']." (".$row['year'].") ".$row['thesis_title']."<h5>";
+                  echo "<h4>Abstract</h4>";
+                  echo "<p>".$row['description']."</p>";
+                    
+                  echo "<table>";
+                  echo "<tr>";
+                  echo "<th>Item Type:</th>"; 
+                  echo "<th>".$row['thesis_types']."</th>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<tr>";
+                  echo "<td>Subjects:</td>";
+                  echo "<td>".$row['subject']."</td>";
+                  echo "</tr>";
+                  echo "<td>Divisions:</td>";  
+                  echo "<td>".$row['departments']."</td>";  
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>ID Code:</td>";
+                  echo "<td>".$row['publication_id']."</td>";
+                  echo "</tr>";
                   
-                <table>
-                  <tr>
-                    <th>Item Type:</th>  
-                    <th>Thesis (PhD)</th> 
-                  </tr>
-                  <tr>
-                    <td>Uncontrolled Keywords:</td>
-                    <td>Performance; Sports Autobiography; Athletic Identity; Family narrative; Choice.</td> 
-                  </tr>
-                  <tr>
-                    <td>Subjects:</td>
-                    <td>Humanities & Social Sciences > Sociology of Communication</td>
-                  </tr>
-                    <td>Divisions:</td>  
-                  <td>Social Sciences > Department of Humanities & Social Sciences</td>  
-                  </tr>
-                  <tr>
-                    <td>ID Code:</td>
-                    <td>10200</td>
-                     </tr>
-                  <tr>
-                    <td>Deposited By:</td>
-                    <td>IR Staff BPCL</td>
-                     </tr>
-                  <tr>
-                    <td>Deposited On:</td>
-                    <td>04 Oct 2021 17:55</td>
-                     </tr>
-                  <tr>
-                    <td>Last Modified:</td>
-                    <td>04 Oct 2021 17:55</td>
-                     </tr><tr>
-                    <td>Related URLs:</td>
-                  <td> http://ethesis.nitrkl.ac.in/10200/</td>
-                     </tr>
-                  <tr>
-                    <td>Supervisor(s):</td>
-                    <td>Mohanty, Seemita</td>
-                     </tr>
+                  echo "<tr>";
+                  echo "<td>Deposited On:</td>";
+                  echo "<td>".$row['date_upload']."</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Last Modified:</td>";
+                  echo "<td>".$row['date_modify']."</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Supervisor(s):</td>";
+                  echo "<td>".$row['supervisors']."</td>";
+                  echo "</tr>";
                   
-                </table>
-                 Repository Staff Only: <a>item control page</a>
-                  </div>
-                <hr style="height:1px;border-top:2px solid grey" />
-                <hr style="height:1px;border-top:2px solid purple" />
-                <img src="https://www.google.com/search?q=nit+jsr+logo&rlz=1C1CHBD_enIN930IN930&sxsrf=AOaemvLYZw-spmeiYVM_UBnzXqoogdszkA:1635076306488&tbm=isch&source=iu&ictx=1&fir=V3pN841luaQvSM%252CgYNcFe1V5c3UDM%252C_%253Bz1GQMF0nDCEPZM%252CnR-5EF_urCjWyM%252C_%253B6B-INzE1z6-fsM%252CiDW7DMTm9kEAnM%252C_%253B4E4wgjqob4CE5M%252CuuPbYZqEvd5H2M%252C_%253BbLhC0U8NCTdpUM%252C9ES9kTQUG15nnM%252C_%253B8ogcbfYka0RyhM%252C0RK_sunYJCCiWM%252C_%253Bb5TZ75CLpAuD-M%252CgG_Xc-7RSOrwJM%252C_%253BXKPRMEEebgKjjM%252ChA8GNr45YkJfQM%252C_%253B_FUSrsDYYqNNgM%252CDH_W_BMYI-_rdM%252C_%253BcvYCVqx4_1UijM%252C8nTZzqglBiyEsM%252C_%253B0IMU2yHK4oYGFM%252CuY2bhksPHJp0nM%252C_%253BPQ9pkCBHUeIkOM%252CF5Q3ypbcZyHQoM%252C_%253BJOM_D6fmsGQNbM%252C8QHG1x03Nn8dnM%252C_%253BHbfqeHh-DbeDBM%252CF5Q3ypbcZyHQoM%252C_%253BhsrNBcHmlDQfHM%252Cn2yv-iY0yp_dPM%252C_%253BX5O-0J55oBzeJM%252C8_9l5BH2n9IZPM%252C_%253Bbgey4LmWw8-nvM%252CnR-5EF_urCjWyM%252C_&vet=1&usg=AI4_-kS2ANPcvxv-WBFudnoLH1Vy-Vvpug&sa=X&sqi=2&ved=2ahUKEwiTy7bz_eLzAhUzrZUCHdchBOwQ9QF6BAgMEAE#imgrc=XKPRMEEebgKjjM">NIT Jamshedpur
+                echo "</table>";
+
+
+                
+                }
+          ?>
 
         </div>
 
