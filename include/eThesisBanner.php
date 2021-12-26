a80939bb46defbe558e63cd937d88653ded550c3
MOHAMMAD SHABBIR ALAM <93095875+imohdalam@users.noreply.github.com>
2021-12-27 01:12:14 +0530

    Add files via upload

---
+++ include/eThesisBanner.php
@@ -0,0 +1,39 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eThesis Central Library</title>
+</head>
+<body>
+<div>
+    <center>
+      <img src="image/eThesisBanner.png" alt="eThesis Banner" width="1519" height="200">
+    </center><br>
+</div>
+<div class="menu">
+            <ul class="ep_tm_menu"> 
+                <li>
+                    <a href="thesis.php">All</a>
+                </li>
+                <li>
+                    <a href="thesis_year.php">Year</a>
+                </li>
+                <li>
+                    <a href="thesis_department.php">Departments</a>
+                </li>
+                <li>
+                    <a href="thesis_subject.php">Subject</a>
+                </li>
+                <li>
+                    <a href="thesis_supervisors.php">Supervisors</a>
+                </li>
+                <li> 
+                    <a href="thesis_type.php">Thesis Type</a>
+                </li>
+            </ul>
+        </div>
+        <br><br><br>
+</body>
+</html>
